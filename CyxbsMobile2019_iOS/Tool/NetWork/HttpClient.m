9f363253d20406ebb08002cf181cea751ca39597
wycbug <wycbug@163.com>
2019-12-07 12:58:00 +0800

    HTTPClient封装添加纯json请求方法，改登录的网络请求为使用HttpClient封装，删除没用的网络请求类

--- CyxbsMobile2019_iOS/Tool/NetWork/HttpClient.m
+++ CyxbsMobile2019_iOS/Tool/NetWork/HttpClient.m
@@ -28,23 +28,54 @@
                 success:(void (^)(NSURLSessionDataTask *task, id responseObject))success
                 failure:(void (^)(NSURLSessionDataTask *task, NSError *error))failure{
     AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
-//    manager.requestSerializer = [AFHTTPRequestSerializer serializer];
-//    manager.requestSerializer = [AFJSONRequestSerializer serializer];
+    //    manager.requestSerializer = [AFHTTPRequestSerializer serializer];
+    //    manager.requestSerializer = [AFJSONRequestSerializer serializer];
     // 以json格式向服务器发送请求
     AFJSONResponseSerializer *serializer = [AFJSONResponseSerializer serializer];
     [serializer setRemovesKeysWithNullValues:YES];
     // 处理服务器返回null
-//    [manager.requestSerializer setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+    //    [manager.requestSerializer setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
     [manager setResponseSerializer:serializer];
     manager.responseSerializer.acceptableContentTypes =  [manager.responseSerializer.acceptableContentTypes setByAddingObjectsFromSet:[NSSet setWithObjects:@"application/json", @"text/json", @"text/javascript",@"text/html", @"text/plain",@"application/atom+xml",@"application/xml",@"text/xml",nil]];
     switch (method) {
         case HttpRequestGet:
-//            [manager GET:url parameters:parameters progress:progress success:success failure:failure];
+            //            [manager GET:url parameters:parameters progress:progress success:success failure:failure];
             [manager GET:url parameters:parameters success:success failure:failure];
             break;
         case HttpRequestPost:
             [manager POST:url parameters:parameters success:success failure:failure];
-//            [manager POST:url parameters:parameters progress:progress success:success failure:failure];
+            //            [manager POST:url parameters:parameters progress:progress success:success failure:failure];
+            break;
+        case HttpRequestPut:
+            [manager PUT:url parameters:parameters success:success failure:failure];
+            break;
+        case HttpRequestDelete:
+            [manager DELETE:url parameters:parameters success:success failure:failure];
+            break;
+        default:
+            break;
+    }
+    //超时时间30s
+    manager.requestSerializer.timeoutInterval = 30.0;
+}
+
+
+- (void)requestWithJson:(NSString *)url
+                 method:(NSInteger)method
+             parameters:(id)parameters
+         prepareExecute:(PrepareExecuteBlock) prepare
+               progress:(void (^)(NSProgress * progress))progress
+                success:(void (^)(NSURLSessionDataTask *task, id responseObject))success
+                failure:(void (^)(NSURLSessionDataTask *task, NSError *error))failure{
+    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+    // 请求需要json
+    manager.requestSerializer = [AFJSONRequestSerializer serializer];
+    switch (method) {
+        case HttpRequestGet:
+            [manager GET:url parameters:parameters success:success failure:failure];
+            break;
+        case HttpRequestPost:
+            [manager POST:url parameters:parameters success:success failure:failure];
             break;
         case HttpRequestPut:
             [manager PUT:url parameters:parameters success:success failure:failure];
@@ -91,12 +122,12 @@
     manager.responseSerializer.acceptableContentTypes =  [manager.responseSerializer.acceptableContentTypes setByAddingObjectsFromSet:[NSSet setWithObjects:@"application/json", @"text/json", @"text/javascript",@"text/html", @"text/plain",@"application/atom+xml",@"application/xml",@"text/xml",nil]];
     switch (method) {
         case HttpRequestGet:
-
+            
             [manager GET:url parameters:parameters success:success failure:failure];
             break;
         case HttpRequestPost:
             [manager POST:url parameters:parameters success:success failure:failure];
-
+            
             break;
         case HttpRequestPut:
             [manager PUT:url parameters:parameters success:success failure:failure];
