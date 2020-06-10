7eec5b0825e33fde198eb40302e833a24b353898
qizhemotuosangeyan <414772073@qq.com>
2020-06-10 17:10:07 +0800

    修复了电费查询绑定后不显示各种费用的bug，缓存了绑定宿舍后下一次点击绑定页面的默认宿舍

--- CyxbsMobile2019_iOS/Discover/Discover_MainPage/Model/PickerView/PickerModel.h
+++ CyxbsMobile2019_iOS/Discover/Discover_MainPage/Model/PickerView/PickerModel.h
@@ -23,8 +23,8 @@ NS_ASSUME_NONNULL_BEGIN
 
 ///根据苑名字和几舍获取栋数字：例如参数1：知行苑，参数2：8舍，返回：16栋
 -(NSString *)getNumberOfDormitoryWith: (NSString*)building andPlace:(NSString*)place;
-
-
+/// 根据几栋获得是什么宿舍，几舍。例如参数：24栋，返回：@[1,0]//1是明理苑的索引，0是1舍的索引
+-(NSArray <NSNumber*>*)getBuildingNameIndexAndBuildingNumberIndexByNumberOfDormitory: (NSString *)dormitoryNumber;
 @end
 
 NS_ASSUME_NONNULL_END
