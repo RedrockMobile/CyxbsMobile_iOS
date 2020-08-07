62155b7ae110a0a6005a8f0d2c2797f38630386c
fyhNB <1374872604@qq.com>
2020-08-07 17:11:47 +0800

    新增：回答页面深色模式适配，回答和提问界面的删除和大图浏览

--- CyxbsMobile2019_iOS/QA/QAAsk/QAAskViewController.m
+++ CyxbsMobile2019_iOS/QA/QAAsk/QAAskViewController.m
@@ -11,6 +11,8 @@
 #import "QAAskNextStepView.h"
 #import "QAAskIntegralPickerView.h"
 #import "QAExitView.h"
+#import "GKPhotoBrowser.h"
+
 @interface QAAskViewController ()<UITextViewDelegate,UIImagePickerControllerDelegate,UINavigationControllerDelegate>
 
 @property(assign,nonatomic)BOOL isFromDraft;
@@ -23,8 +25,8 @@
 @property(copy,nonatomic)NSString *kind;
 @property(strong,nonatomic)NSMutableArray *kindBtnArray;
 //添加图片
-@property(strong,nonatomic)NSMutableArray *askImageArray;
-@property(strong,nonatomic)NSMutableArray *askImageViewArray;
+@property(strong,nonatomic)NSMutableArray<UIImage *> *askImageArray;
+@property(strong,nonatomic)NSMutableArray<UIImageView *> *askImageViewArray;
 
 //下一步提示界面
 @property(strong,nonatomic)QAAskNextStepView *nextStepView;
@@ -237,42 +239,54 @@
 - (void)setAddImageView{
     
     NSInteger count = self.askImageArray.count;
-    if (count + 1 <= 6){
+    // 只有小于等于6张图片的时候才添加”添加按钮“
+    if (count + 1 <= 7) {
+        
         UIImageView *addImageView = [[UIImageView alloc]init];
         [addImageView setImage:[UIImage imageNamed:@"addImageButton"]];
+        addImageView.layer.cornerRadius = 8;
+        addImageView.clipsToBounds = YES;
+        addImageView.contentMode = UIViewContentModeScaleAspectFill;
         addImageView.userInteractionEnabled = YES;
-        //    //添加点击手势
+        
+        // 添加点击手势
         UIGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(addImage)];
         [addImageView addGestureRecognizer:tapGesture];
         [self.view addSubview:addImageView];
         
-        
         NSInteger widthAndHeight = (SCREEN_WIDTH - 60)/3;
-        if (count < 3){
+        
+        // 如果“添加按钮”（其实是个图片）和其他图片总数小于7(0~5张图片)，那么将“添加按钮”加在其他图片后面。
+        if (count + 1 < 7) {
+            if (count < 3){
+                [addImageView mas_makeConstraints:^(MASConstraintMaker *make) {
+                    make.left.mas_equalTo(self.view.mas_left).mas_offset(20 + (widthAndHeight + 10) * count);
+                    make.top.mas_equalTo(self.askTextView.mas_bottom).mas_offset(10);
+                    make.height.width.mas_equalTo(widthAndHeight);
+                }];
+            } else {
+                [addImageView mas_makeConstraints:^(MASConstraintMaker *make) {
+                    make.left.mas_equalTo(self.view.mas_left).mas_offset(20 + (widthAndHeight + 10) * (count - 3));
+                    make.top.mas_equalTo(self.askTextView.mas_bottom).mas_offset(widthAndHeight + 20);
+                    make.height.width.mas_equalTo(widthAndHeight);
+                }];
+            }
+        } else if (count + 1 == 7) {        // 已经有6张图片了，“添加按钮”就不加在前面的图片后面了，而是直接加在第6张上面
+            addImageView.alpha = 0.85;
             [addImageView mas_makeConstraints:^(MASConstraintMaker *make) {
-                make.left.mas_equalTo(self.view.mas_left).mas_offset(20 + (widthAndHeight + 10) * count);
-                make.top.mas_equalTo(self.askTextView.mas_bottom).mas_offset(10);
-                make.height.width.mas_equalTo(widthAndHeight);
-                
-            }];
-        }else{
-            [addImageView mas_makeConstraints:^(MASConstraintMaker *make) {
-                make.left.mas_equalTo(self.view.mas_left).mas_offset(20 + (widthAndHeight + 10) * (count - 3));
+                make.left.mas_equalTo(self.view.mas_left).mas_offset(20 + (widthAndHeight + 10) * 2);
                 make.top.mas_equalTo(self.askTextView.mas_bottom).mas_offset(widthAndHeight + 20);
                 make.height.width.mas_equalTo(widthAndHeight);
-                
             }];
         }
         [self.askImageViewArray addObject:addImageView];
     }
-    
 }
 - (void)addImage{
     
     UIImagePickerController *imagePicker = [[UIImagePickerController alloc] init];
     imagePicker.delegate = self;
-    imagePicker.modalPresentationStyle = UIModalPresentationFullScreen;
-//    imagePicker.allowsEditing = YES;//编辑模式  但是编辑框是正方形的
+    imagePicker.allowsEditing = NO;//编辑模式  但是编辑框是正方形的
     
     // 设置可用的媒体类型、默认只包含kUTTypeImage
     imagePicker.mediaTypes = @[(NSString *)kUTTypeMovie, (NSString *)kUTTypeImage];
@@ -306,19 +320,115 @@
     
     [self presentViewController:actionSheet animated:YES completion:nil];
     
-    
 }
+
+
+- (void)changeImage:(UITapGestureRecognizer *)sender {
+    UIAlertController *actionSheet = [UIAlertController alertControllerWithTitle:nil message:nil preferredStyle:UIAlertControllerStyleActionSheet];
+    
+    UIAlertAction *showBigImage = [UIAlertAction actionWithTitle:@"查看大图" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        
+        NSMutableArray *photos = [NSMutableArray array];
+        NSInteger index = 0;
+        
+        for (int i = 0; i < self.askImageArray.count; i++) {
+            UIImage *img = self.askImageArray[i];
+            if (((UIImageView *)sender.view).image == img) {
+                index = i;
+            }
+            GKPhoto *photo = [GKPhoto new];
+            photo.image = img;
+            [photos addObject:photo];
+        }
+        GKPhotoBrowser *browser = [GKPhotoBrowser photoBrowserWithPhotos:photos currentIndex:index];
+        browser.showStyle = GKPhotoBrowserShowStylePush;
+        [browser showFromVC:self];
+    }];
+    
+    UIAlertAction *deleteAction = [UIAlertAction actionWithTitle:@"删除图片" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        
+        [actionSheet dismissViewControllerAnimated:YES completion:^{
+            
+            UIAlertController *warningAlertController = [UIAlertController alertControllerWithTitle:@"删除图片" message:@"确定要删除图片吗？" preferredStyle:UIAlertControllerStyleAlert];
+            
+            UIAlertAction *determineToDelete = [UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDestructive handler:^(UIAlertAction * _Nonnull action) {
+                [self.askImageArray removeObject:((UIImageView *)(sender.view)).image];
+                NSLog(@"%@--%@", self.askImageArray, self.askImageViewArray);
+                
+                // 删除以后，图片需要往前移动。如果ImageArray还剩余6张及以上的图片，view不用移动，只需要将上面的image全部往前移动，else 是ImageView需要移动的情况
+                if (self.askImageArray.count >= 6) {
+                    for (int i = 0; i < 6; i++) {
+                        self.askImageViewArray[i].image = self.askImageArray[i];
+                    }
+                } else {
+                    [self.askImageViewArray removeObject:(UIImageView *)sender.view];
+                    [sender.view removeFromSuperview];
+                    // 删除图片以后全部重新约束
+                    NSInteger widthAndHeight = (SCREEN_WIDTH - 60)/3;
+                    for (int i = 0; i < self.askImageViewArray.count; i++) {
+                        if (i < 3) {        // 前三个图片的约束
+                            [self.askImageViewArray[i] mas_remakeConstraints:^(MASConstraintMaker *make) {
+                                make.left.mas_equalTo(self.view.mas_left).mas_offset(20 + (widthAndHeight + 10) * i);
+                                make.top.mas_equalTo(self.askTextView.mas_bottom).mas_offset(10);
+                                make.height.width.mas_equalTo(widthAndHeight);
+                            }];
+                        } else if (i < 6) { // 4, 5, 6的约束
+                            [self.askImageViewArray[i] mas_remakeConstraints:^(MASConstraintMaker *make) {
+                                make.left.mas_equalTo(self.view.mas_left).mas_offset(20 + (widthAndHeight + 10) * (i - 3));
+                                make.top.mas_equalTo(self.askTextView.mas_bottom).mas_offset(widthAndHeight + 20);
+                                make.height.width.mas_equalTo(widthAndHeight);
+                            }];
+                        } else if (i == self.askImageViewArray.count - 1) {  // 最后一个，是添加按钮。
+                            [self.askImageViewArray[i] mas_remakeConstraints:^(MASConstraintMaker *make) {
+                                make.left.mas_equalTo(self.view.mas_left).mas_offset(20 + (widthAndHeight + 10) * 2);
+                                make.top.mas_equalTo(self.askTextView.mas_bottom).mas_offset(widthAndHeight + 20);
+                                make.height.width.mas_equalTo(widthAndHeight);
+                            }];
+                        }
+                        
+                        [((UIView *)(self.askImageViewArray[i])) layoutIfNeeded];
+                    }       // for 循环的后括号
+                }       // else 的后括号
+            }];     // ”确定删除图片“ alertAction 的后括号
+            
+            UIAlertAction *cancel = [UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:nil];
+            
+            [warningAlertController addAction:determineToDelete];
+            [warningAlertController addAction:cancel];
+            
+            [self presentViewController:warningAlertController animated:YES completion:nil];
+        }];  // dismiss alertSheet completion 的后括号
+    }];  // ”删除图片“ alertAction 的后括号
+    
+    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:nil];
+    
+    [actionSheet addAction:showBigImage];
+    [actionSheet addAction:deleteAction];
+    [actionSheet addAction:cancelAction];
+    
+    [self presentViewController:actionSheet animated:YES completion:nil];
+}
+
+
+
 #pragma mark - =======UIImagePickerControllerDelegate=========
 
 - (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
     
     [picker dismissViewControllerAnimated:YES completion:nil];
     UIImage *image = [info objectForKey:UIImagePickerControllerOriginalImage];
-    //    self.askImageArray[0] = image;
     [self.askImageArray addObject:image];
-    UIImageView *imgView = [self.askImageViewArray lastObject];
-    [imgView setImage:image];
-    [self setAddImageView];
+    if (self.askImageArray.count <= 6) {     // 第6张以后的图片都不显示在界面上，查看大图可以查看
+        UIImageView *imgView = [self.askImageViewArray lastObject];
+        [imgView setImage:image];
+    
+        // 在调用 “setAddImageView” 之前，将当前正在设置的 imageView 的手势方法改成查看大图，并且增加长按删除功能。
+        [imgView removeGestureRecognizer:imgView.gestureRecognizers[0]];
+        UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(changeImage:)];
+        [imgView addGestureRecognizer:tap];
+        
+        [self setAddImageView];
+    }
     
 }
 // 取消图片选择调用此方法
