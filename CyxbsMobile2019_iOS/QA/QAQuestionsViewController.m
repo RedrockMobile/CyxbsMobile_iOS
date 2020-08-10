bd648a5c43cf67c4b4b0a463902a05d322af975d
fyhNB <1374872604@qq.com>
2020-08-10 16:07:30 +0800

    修复：邮问和签到页面周数下标越界问题

--- CyxbsMobile2019_iOS/QA/QAQuestionsViewController.m
+++ CyxbsMobile2019_iOS/QA/QAQuestionsViewController.m
@@ -121,10 +121,13 @@
     NSInteger nowTime = [now timeIntervalSince1970];
     double day = (float)(nowTime - beginTime)/(float)86400/(float)7;
     NSInteger nowWeek = (int)ceil(day) - 1;
-    if(nowWeek < 0){
+    
+    NSArray *weekNumArray = @[@"第一周",@"第二周",@"第三周",@"第四周",@"第五周",@"第六周",@"第七周",@"第八周",@"第九周",@"第十周",@"第十一周",@"第十二周",@"第十三周",@"第十四周",@"第十五周",@"第十六周",@"第十七周",@"第十八周",@"第十九周",@"第二十周",@"二十一周",@"二十二周",@"二十三周",@"二十四周",@"二十五周"];
+    
+    if (nowWeek < 0 || nowWeek >= weekNumArray.count) {
         nowWeek = 0;
     }
-    NSArray *weekNumArray = @[@"第一周",@"第二周",@"第三周",@"第四周",@"第五周",@"第六周",@"第七周",@"第八周",@"第九周",@"第十周",@"第十一周",@"第十二周",@"第十三周",@"第十四周",@"第十五周",@"第十六周",@"第十七周",@"第十八周",@"第十九周",@"第二十周",@"二十一周",@"二十二周",@"二十三周",@"二十四周",@"二十五周"];
+    
     NSString *weekNum = weekNumArray[nowWeek];
     NSString *weekStr = [NSString stringWithFormat:@"%@ %@",weekNum,weekDayStr];
     return weekStr;
