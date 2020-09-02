987eb7ac28f0baafe0a441f044a9118e62754603
fyhNB <1374872604@qq.com>
2020-09-02 15:59:41 +0800

    修改开学日期

--- CyxbsMobile2019_iOS/ClassSchedule/Controller/WYCClassBookViewController.m
+++ CyxbsMobile2019_iOS/ClassSchedule/Controller/WYCClassBookViewController.m
@@ -212,7 +212,7 @@
 - (DateModle *)dateModel{
     if(_dateModel==nil){//@"2020-09-07"
         //@"2020-08-24" @"2020-07-20" DateStart
-        _dateModel = [DateModle initWithStartDate:@"2020-07-20"];
+        _dateModel = [DateModle initWithStartDate:DateStart];
     }
     return _dateModel;
 }
