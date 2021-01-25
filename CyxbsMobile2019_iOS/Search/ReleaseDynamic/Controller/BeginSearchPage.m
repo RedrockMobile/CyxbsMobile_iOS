7b240ad6052ceb2d454c6f22a9a6ab529d10f680
Rock-Connotation <2125387192@qq.com>
2021-01-25 17:05:41 +0800

    添加了一些东西

---
+++ CyxbsMobile2019_iOS/Search/ReleaseDynamic/Controller/BeginSearchPage.m
@@ -0,0 +1,38 @@
+//
+//  BeginSearchPage.m
+//  CyxbsMobile2019_iOS
+//
+//  Created by 石子涵 on 2021/1/21.
+//  Copyright © 2021 Redrock. All rights reserved.
+//
+
+#import "BeginSearchPage.h"
+
+@interface BeginSearchPage ()
+
+@end
+
+@implementation BeginSearchPage
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [UIView animateWithDuration:3 animations:^{
+            //
+    }];
+    
+    UIView animateWithDuration:<#(NSTimeInterval)#> delay:<#(NSTimeInterval)#> usingSpringWithDamping:<#(CGFloat)#> initialSpringVelocity:<#(CGFloat)#> options:<#(UIViewAnimationOptions)#> animations:<#^(void)animations#> completion:<#^(BOOL finished)completion#>
+    
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
