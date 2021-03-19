1c3ee50e4dff6c1d3a60eccd75b23bfff1c0b9b0
YYZ4869 <1915648497@qq.com>
2021-03-19 17:58:59 +0800

    完成圈子广场

---
+++ CyxbsMobile2019_iOS/NewQA/YYZTopicGroup/Controller/YYZTopicDetailVC.m
@@ -0,0 +1,33 @@
+//
+//  YYZTopicDetailVC.m
+//  CyxbsMobile2019_iOS
+//
+//  Created by 杨远舟 on 2021/3/16.
+//  Copyright © 2021 Redrock. All rights reserved.
+//
+
+#import "YYZTopicDetailVC.h"
+
+@interface YYZTopicDetailVC ()
+
+@end
+
+@implementation YYZTopicDetailVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor redColor];
+    
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
