91dd7de41e86c963254f456a718fa8c92c99fc6f
mgd666 <1291788563@qq.com>
2021-03-20 21:59:07 +0800

    [feature]请求未查看消息数

---
+++ CyxbsMobile2019_iOS/NewQA/Model/NewCountModel.m
@@ -0,0 +1,24 @@
+//
+//  NewCountModel.m
+//  CyxbsMobile2019_iOS
+//
+//  Created by 阿栋 on 2021/3/20.
+//  Copyright © 2021 Redrock. All rights reserved.
+//
+
+#import "NewCountModel.h"
+
+@implementation NewCountModel
+
+- (void)queryNewCountWithTimestamp:(NSString *)timestamp {
+    HttpClient *client = [HttpClient defaultClient];
+    NSDictionary *param = @{@"last":timestamp};
+    [client requestWithPath:NEW_QA_QUERYNEWCOUNT method:HttpRequestPost parameters:param prepareExecute:nil progress:nil success:^(NSURLSessionDataTask *task, id responseObject) {
+        NSArray *array = [NSArray arrayWithArray:responseObject[@"data"]];
+        NSLog(@"未查看消息数%lu",(unsigned long)array.count);
+    } failure:^(NSURLSessionDataTask *task, NSError *error) {
+    
+    }];
+}
+
+@end
