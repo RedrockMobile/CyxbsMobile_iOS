80c95ffa14af5ae9d60c9f2ae4b6cd5fe8c5282a
YYZ4869 <1915648497@qq.com>
2021-03-21 18:52:10 +0800

    优化圈子详情页

--- CyxbsMobile2019_iOS/ClassSchedule/Model/WYCClassAndRemindDataModel.m
+++ CyxbsMobile2019_iOS/ClassSchedule/Model/WYCClassAndRemindDataModel.m
@@ -68,7 +68,7 @@
 - (void)getPersonalClassBookArrayFromNet:(NSString *)stuNum{
     NSDictionary *paramDict = @{@"stu_num":stuNum};
     
-    [self.afhttpSeMan GET:kebiaoAPI parameters:paramDict success:^(NSURLSessionDataTask * _Nonnull task, id  _Nonnull responseObject) {
+    [self.afhttpSeMan POST:kebiaoAPI parameters:paramDict success:^(NSURLSessionDataTask * _Nonnull task, id  _Nonnull responseObject) {
         CCLog(@"ke%@",responseObject);
         //拿到课表数据
         NSArray *rowLessonDataArr = responseObject[@"data"];
