423c0fbee0c9101c1aeb8da16119778cd24c6aaa
KDG2002 <1398742714@qq.com>
2021-08-26 06:46:24 +0800

    完善界面

--- CyxbsMobile2019_iOS/Mine/FeedBackCenter/MainPage/Views/CustomView/FeedBackView/FeedBackView.h
+++ CyxbsMobile2019_iOS/Mine/FeedBackCenter/MainPage/Views/CustomView/FeedBackView/FeedBackView.h
@@ -10,8 +10,32 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface FeedBackView : UIView
+@interface FeedBackView : UIView <UITextFieldDelegate,UITextViewDelegate>
 
+///标题
+@property (nonatomic,strong) UITextField *heading;
+///分割线
+@property (nonatomic,strong) UIView *splitLine;
+///反馈主内容
+@property (nonatomic,strong) UITextView *feedBackMain;
+///TextView的placeholder
+@property (nonatomic,strong) UILabel *placeholder;
+///标题计数Lbl
+@property (nonatomic,strong) UILabel *headingCountLbl;
+///正文计数Lbl
+@property (nonatomic,strong) UILabel *textCountLbl;
+///分割线2
+@property (nonatomic,strong) UIView *splitLine2;
+///图片提示Lbl
+@property (nonatomic,strong) UILabel *photoLbl;
+///图片数量Lbl
+@property (nonatomic,strong) UILabel *photoCountLbl;
+///1号位图片
+@property (nonatomic,strong) UIImageView *imageView1;
+///2号位图片
+@property (nonatomic,strong) UIImageView *imageView2;
+///3号位图片
+@property (nonatomic,strong) UIImageView *imageView3;
 @end
 
 NS_ASSUME_NONNULL_END
--- CyxbsMobile2019_iOS/Mine/FeedBackCenter/MainPage/Views/CustomView/FeedBackView/FeedBackView.m
+++ CyxbsMobile2019_iOS/Mine/FeedBackCenter/MainPage/Views/CustomView/FeedBackView/FeedBackView.m
@@ -7,7 +7,7 @@
 //
 
 #import "FeedBackView.h"
-
+#import "UIView+XYView.h"
 @implementation FeedBackView
 
 - (instancetype)initWithFrame:(CGRect)frame
@@ -16,8 +16,141 @@
     if (self) {
         self.layer.cornerRadius = 8;
         self.backgroundColor = [UIColor colorNamed:@"#FFFFFF"];
+        [self addSubview:self.feedBackMain];
+        [self addSubview:self.splitLine];
+        [self addSubview:self.heading];
+        [self.feedBackMain addSubview:self.placeholder];
+        [self addSubview:self.textCountLbl];
+        [self addSubview:self.headingCountLbl];
+        [self addSubview:self.splitLine2];
+        [self addSubview:self.photoLbl];
+        [self addSubview:self.photoCountLbl];
+        [self addSubview:self.imageView1];
     }
     return self;
 }
 
+#pragma mark - getter
+- (UITextField *)heading{
+    if (!_heading) {
+        _heading = [[UITextField alloc]initWithFrame:CGRectMake(18, 20, self.width - 75, 25)];
+        _heading.placeholder = @"添加标题";
+        _heading.font = [UIFont fontWithName:PingFangSCBold size:18];
+        _heading.delegate = self;
+        [_heading addTarget:self action:@selector(getlength:) forControlEvents:UIControlEventEditingChanged];
+        [_heading setTextColor:[UIColor colorNamed:@"#15315B"]];
+        Ivar ivar =  class_getInstanceVariable([UITextField class], "_placeholderLabel");
+        UILabel *placeholderLabel = object_getIvar(self.heading, ivar);
+        placeholderLabel.textColor = [UIColor colorNamed:@"heading"];
+    }
+    return _heading;
+}
+
+- (UIView *)splitLine{
+    if (!_splitLine) {
+        _splitLine = [[UIView alloc]initWithFrame:CGRectMake(16, 55, self.width - 32, 1)];
+        _splitLine.backgroundColor = [UIColor systemGray5Color];
+    }
+    return _splitLine;
+}
+
+- (UITextView *)feedBackMain{
+    if (!_feedBackMain) {
+        _feedBackMain = [[UITextView alloc]initWithFrame:CGRectMake(14, 65, self.width - 36, 280)];
+        _feedBackMain.delegate = self;
+        [_feedBackMain setTextColor:[UIColor colorNamed:@"#15315B"]];
+        _feedBackMain.font = [UIFont fontWithName:PingFangSCMedium size:15];
+    }
+    return _feedBackMain;
+}
+
+- (UILabel *)placeholder{
+    if (!_placeholder) {
+        _placeholder = [[UILabel alloc]initWithFrame:CGRectMake(5, 2, 200, 30)];
+        _placeholder.textColor = [UIColor colorNamed:@"heading"];
+        _placeholder.text = @"添加问题描述";
+        _placeholder.font = [UIFont fontWithName:PingFangSCMedium size:15];
+    }
+    return _placeholder;
+}
+
+- (UILabel *)headingCountLbl{
+    if (!_headingCountLbl) {
+        _headingCountLbl = [[UILabel alloc]initWithFrame:CGRectMake(315, 25, 15, 17)];
+        _headingCountLbl.font = [UIFont fontWithName:PingFangSCMedium size:13];
+        _headingCountLbl.textColor = [UIColor colorNamed:@"Count"];
+        _headingCountLbl.text = @"0";
+    }
+    return _headingCountLbl;
+}
+
+- (UILabel *)textCountLbl{
+    if (!_textCountLbl) {
+        _textCountLbl = [[UILabel alloc]initWithFrame:CGRectMake(293, 317, 35, 17)];
+        _textCountLbl.font = [UIFont fontWithName:PingFangSCMedium size:12];
+        _textCountLbl.textColor = [UIColor colorNamed:@"Count"];
+        _textCountLbl.text = @"0/200";
+    }
+    return _textCountLbl;
+}
+- (UIView *)splitLine2{
+    if (!_splitLine2) {
+        _splitLine2 = [[UIView alloc]initWithFrame:CGRectMake(16, 344, self.width - 32, 1)];
+        _splitLine2.backgroundColor = [UIColor systemGray5Color];
+    }
+    return _splitLine2;
+}
+
+- (UILabel *)photoLbl{
+    if (!_photoLbl) {
+        _photoLbl = [[UILabel alloc]initWithFrame:CGRectMake(18, 361, 150, 21)];
+        _photoLbl.text = @"相关问题的截图或图片";
+        _photoLbl.font = [UIFont fontWithName:PingFangSCMedium size:15];
+        _photoLbl.textColor = [UIColor colorNamed:@"photo"];
+    }
+    return _photoLbl;
+}
+
+- (UILabel *)photoCountLbl{
+    if (!_photoCountLbl) {
+        _photoCountLbl = [[UILabel alloc]initWithFrame:CGRectMake(304, 363, 21, 17)];
+        _photoCountLbl.font = [UIFont fontWithName:PingFangSCMedium size:12];
+        _photoCountLbl.textColor = [UIColor colorNamed:@"Count"];
+        _photoCountLbl.text = @"0/3";
+    }
+    return _photoCountLbl;
+}
+
+- (UIImageView *)imageView1{
+    if (!_imageView1) {
+        _imageView1 = [[UIImageView alloc]initWithFrame:CGRectMake(18, 389, 98, 98)];
+        UIImageView *i = [[UIImageView alloc]initWithFrame:CGRectMake(34, 32.5, 30, 30)];
+        i.image = [UIImage imageNamed:@"myplus"];
+        _imageView1.image = [UIImage imageNamed:@"plusBG"];
+        [_imageView1 addSubview:i];
+    }
+    return _imageView1;
+}
+
+#pragma mark - 其他方法
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    [self endEditing:YES];
+}
+
+- (void)getlength:(UITextField *)sender{
+    NSLog(@"%lu",(unsigned long)sender.text.length);
+    self.headingCountLbl.text = [NSString stringWithFormat:@"%lu",(unsigned long)sender.text.length];
+}
+
+#pragma mark - textView代理
+- (void)textViewDidChange:(UITextView *)textView{
+    if (textView.text.length != 0) {
+        self.placeholder.hidden = YES;
+        self.textCountLbl.text = [NSString stringWithFormat:@"%lu/200",(unsigned long)textView.text.length];
+    }
+    else{
+        self.placeholder.hidden = NO;
+        self.textCountLbl.text = @"0/200";
+    }
+}
 @end
