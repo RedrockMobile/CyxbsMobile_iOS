b42b7659533c2453d11c62165cd4027bc4371948
iced2020 <67853803+iced2020@users.noreply.github.com>
2023-10-24 22:25:17 +0800

    [optimize]中心点击图片优化

--- CyxbsMobile2019_iOS/Center/Carnie/VC/CenterVC.m
+++ CyxbsMobile2019_iOS/Center/Carnie/VC/CenterVC.m
@@ -23,13 +23,6 @@
 
 @implementation CenterVC
 
-// 定义按钮标签的枚举
-typedef NS_ENUM(NSInteger, ButtonTag) {
-    ButtonTagFood = 1,
-    ButtonTagBiaoTai = 2,
-    ButtonTagActivityNotify = 3
-};
-
 #pragma mark - Life Cycle
 
 - (void)viewWillAppear:(BOOL)animated {
@@ -51,7 +44,7 @@ typedef NS_ENUM(NSInteger, ButtonTag) {
     [self setDetailBtns];
 }
 
-- (void)viewWillDisappear:(BOOL)animated {
+- (void)viewDidDisappear:(BOOL)animated {
     [super viewWillDisappear:animated];
     ((ClassTabBar *)self.tabBarController.tabBar).hidden = NO;
 //    // 恢复背景颜色
@@ -103,53 +96,22 @@ typedef NS_ENUM(NSInteger, ButtonTag) {
 
 // 按钮设置方法
 - (void)setDetailBtns {
-    // 设置按钮标签(tag)
-    self.centerView.foodBtn.tag = ButtonTagFood;
-    self.centerView.biaoTaiBtn.tag = ButtonTagBiaoTai;
-    self.centerView.activityNotifyBtn.tag = ButtonTagActivityNotify;
-    
     // 设置按钮触发事件
-    [self.centerView.foodBtn addTarget:self action:@selector(pushDetailVC:) forControlEvents:UIControlEventTouchUpInside];
-    [self.centerView.biaoTaiBtn addTarget:self action:@selector(pushDetailVC:) forControlEvents:UIControlEventTouchUpInside];
+    [self.centerView.foodBtn addTarget:self action:@selector(pushFoodVC) forControlEvents:UIControlEventTouchUpInside];
+    [self.centerView.biaoTaiBtn addTarget:self action:@selector(pushAttitudeVC) forControlEvents:UIControlEventTouchUpInside];
+    [self.centerView.activityNotifyBtn addTarget:self action:@selector(pushActivityVC) forControlEvents:UIControlEventTouchUpInside];
     
     // 添加图片点击手势
-    [self addTapGestureToView:self.centerView.foodImg withTag:ButtonTagFood];
-    [self addTapGestureToView:self.centerView.biaoTaiImg withTag:ButtonTagBiaoTai];
+    [self addTapGestureToView:self.centerView.foodImg withSelector:@selector(pushFoodVC)];
+    [self addTapGestureToView:self.centerView.biaoTaiImg withSelector:@selector(pushAttitudeVC)];
+    [self addTapGestureToView:self.centerView.activityNotifyImg withSelector:@selector(pushActivityVC)];
 }
 
 // 添加点击手势
-- (void)addTapGestureToView:(UIView *)view withTag:(NSInteger)tag {
+- (void)addTapGestureToView:(UIView *)view withSelector:(SEL)selector {
     view.userInteractionEnabled = YES;
-    UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(pushDetailVC:)];
-    view.tag = tag;
+    UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:selector];
     [view addGestureRecognizer:tapGesture];
-}
-
-// 按钮触碰事件
-- (void)pushDetailVC:(id)sender {
-    NSInteger tag = 0;
-    if ([sender isKindOfClass:[UIButton class]]) {
-        tag = ((UIButton *)sender).tag;
-    } else if ([sender isKindOfClass:[UITapGestureRecognizer class]]) {
-        tag = ((UIGestureRecognizer *)sender).view.tag;
-    }
-    
-    switch (tag) {
-        case ButtonTagFood: {
-            [self pushFoodVC];
-            break;
-        }
-        case ButtonTagBiaoTai: {
-            [self pushAttitudeVC];
-            break;
-        }
-        case ButtonTagActivityNotify: {
-            // 处理活动布告按钮的逻辑
-            break;
-        }
-        default:
-            break;
-    }
 }
 
 // 跳转到美食
@@ -166,6 +128,10 @@ typedef NS_ENUM(NSInteger, ButtonTag) {
     attitudeVC.hidesBottomBarWhenPushed = YES;
     self.navigationController.navigationBarHidden = YES;
     [self.navigationController pushViewController:attitudeVC animated:YES];
+}
+
+// 跳转到活动
+- (void)pushActivityVC {
 }
 
 #pragma mark - Getter
