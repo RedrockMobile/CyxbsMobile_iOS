10cb70effcae45d83f2b53dab4b950ea32aebb7f
iced2020 <67853803+iced2020@users.noreply.github.com>
2023-11-14 13:44:14 +0800

    [bugfix]请求添加 token

--- NetWork/SessionManager.swift
+++ NetWork/SessionManager.swift
@@ -65,7 +65,7 @@ class SessionManager: Session {
     
     static let shared = SessionManager(serverTrustManager: TrustPlicyManager(allHostsMustBeEvaluated: true, evaluators: [:]))
     
-    var token: String?
+    var token: String? = UserModel.default.token?.token
     
     @discardableResult
     open func ry_request(_ convertible: URLConvertible,
