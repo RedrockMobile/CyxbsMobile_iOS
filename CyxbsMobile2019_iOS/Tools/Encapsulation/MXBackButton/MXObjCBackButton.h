3f71544703d2e801a0639a726277512f5ac10ca6
Max Xu <114380848+Max-Xu2004@users.noreply.github.com>
2024-03-15 20:49:26 +0800

    [feature]自定义返回按钮类，解决返回按钮热区太小问题

---
+++ CyxbsMobile2019_iOS/Tools/Encapsulation/MXBackButton/MXObjCBackButton.h
@@ -0,0 +1,26 @@
+//
+//  MXBackButtonObjC.h
+//  CyxbsMobile2019_iOS
+//
+//  Created by Max Xu on 2024/3/15.
+//  Copyright © 2024 Redrock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MXObjCBackButton : UIButton
+
+@property (nonatomic, assign) BOOL isAutoHotspotExpand;
+
+/**
+ isAutoHotspotExpand为是否自动扩大点击热区，默认关闭。
+ 如果开启，并且按钮width 或 height 小于44 ，则将相应的小于44的
+ 边长的点击范围设置为44
+ */
+- (instancetype)initWithFrame:(CGRect)frame isAutoHotspotExpand:(BOOL)isAutoHotspotExpand;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ CyxbsMobile2019_iOS/Tools/Encapsulation/MXBackButton/MXObjCBackButton.m
@@ -0,0 +1,51 @@
+//
+//  MXBackButtonObjC.m
+//  CyxbsMobile2019_iOS
+//
+//  Created by Max Xu on 2024/3/15.
+//  Copyright © 2024 Redrock. All rights reserved.
+//
+
+#import "MXObjCBackButton.h"
+
+@implementation MXObjCBackButton
+
+- (instancetype)initWithFrame:(CGRect)frame isAutoHotspotExpand:(BOOL)isAutoHotspotExpand {
+    self = [super initWithFrame:frame];
+    if (self) {
+        self.isAutoHotspotExpand = isAutoHotspotExpand;
+        [self setImage:[UIImage imageNamed:@"activityBack"] forState:UIControlStateNormal];
+        self.imageView.contentMode = UIViewContentModeScaleAspectFit;
+        self.contentHorizontalAlignment = UIControlContentHorizontalAlignmentCenter;
+        self.contentVerticalAlignment = UIControlContentVerticalAlignmentCenter;
+    }
+    return self;
+}
+
+- (BOOL)pointInside:(CGPoint)point withEvent:(UIEvent *)event {
+    [super pointInside:point withEvent:event];
+    CGFloat top = 0;
+    CGFloat left = 0;
+    CGFloat bottom = 0;
+    CGFloat right = 0;
+    if (self.isAutoHotspotExpand) {
+        if (self.frame.size.height < 44) {
+            top = (44 - self.frame.size.height)/2.0;
+            bottom = (44 - self.frame.size.height)/2.0;
+        }
+        if (self.frame.size.width < 44) {
+            left = (44 - self.frame.size.width)/2.0;
+            right = (44 - self.frame.size.width)/2.0;
+        }
+    }
+    //添加负号使相应的内边距变为向外扩展
+    UIEdgeInsets hotspotExpandEdge = UIEdgeInsetsMake(- top, - left, - bottom, -right);
+    if(UIEdgeInsetsEqualToEdgeInsets(hotspotExpandEdge, UIEdgeInsetsZero) ||       !self.enabled || self.hidden) {
+        return [super pointInside:point withEvent:event];
+    }
+    CGRect relativeFrame = self.bounds;
+    CGRect hotspot = UIEdgeInsetsInsetRect(relativeFrame, hotspotExpandEdge);
+    return CGRectContainsPoint(hotspot, point);
+}
+
+@end
